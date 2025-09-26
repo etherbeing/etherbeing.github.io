56d072a2f6dc76f9c8d285ceaf98386d6918f233
Esteban Chacon Martin <etherbeing99@proton.me>
2025-09-26 14:05:24 -0400

    fix: make the backend to be exposed to the internet

--- api/src/main.rs
+++ api/src/main.rs
@@ -69,7 +69,7 @@ async fn main() -> std::io::Result<()> {
                     .url("/api-doc/openapi.json", ApiDoc::openapi()),
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
