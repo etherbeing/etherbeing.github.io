3191c03a2c36d7d43c9733d8cf626df3cac235f2
Esteban Chacon Martin <etherbeing99@proton.me>
2025-09-26 11:16:56 -0400

    fix: Ready for cold compilation

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -2,7 +2,7 @@ use actix_cors::Cors;
 
 pub fn setup_cors() -> Cors {
     let mut cors = Cors::default().allow_any_method().allow_any_header();
-    let frontend_urls = &std::env::var("FRONTEND_URL").unwrap_or_else(|err| {
+    let frontend_urls = &std::env::var("FRONTEND_URL").unwrap_or_else(|_| {
         panic!("Frontend URLs aren't setup so nothing will be usable from browsers due to CORS")
     });
     for frontend_url in frontend_urls.split(",").collect::<Vec<&str>>() {
