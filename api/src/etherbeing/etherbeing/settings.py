ba2b010d0abb5a613dd987b1943942fc1beb68c2
Esteban Chacon Martin <etherbeing99@proton.me>
2025-12-15 03:37:15 -0500

    fix: use whitenoise

--- api/src/etherbeing/etherbeing/settings.py
+++ api/src/etherbeing/etherbeing/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # Add WhiteNoise here
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -133,6 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 CACHES = {
     "default": {
@@ -143,4 +145,10 @@ CACHES = {
             # Add 'PASSWORD': 'your-redis-password' if you set one
         },
     }
+}
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
 }
