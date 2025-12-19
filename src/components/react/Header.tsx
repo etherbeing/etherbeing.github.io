c05ccf0110cd1d9884d22de9615ae7b4ee2bdbe8
Esteban Chacon Martin <etherbeing99@proton.me>
2025-12-18 23:28:52 -0500

    feat: Improve design and enhanced header

--- src/components/react/Header.tsx
+++ src/components/react/Header.tsx
@@ -4,14 +4,13 @@ import SpotlightCard from "./SpotlightCard";
 export default function Header() {
   return (
     <header className="z-20 fixed w-screen flex items-center justify-center mt-10">
-      <div className="absolute max-w-4xl rounded-3xl  z-15 w-full h-full backdrop-blur-2xl"></div>
-      <SpotlightCard className="z-20 max-w-4xl h-20 w-full flex justify-between bg-transparent">
+      <div className="absolute max-w-[95%] md:max-w-4xl rounded-3xl z-15 w-full h-full backdrop-blur-2xl"></div>
+      <SpotlightCard className="z-20 max-w-[95%] md:max-w-4xl h-20 w-full flex justify-between bg-transparent">
         <a href="/" className="flex gap-3 justify-center items-center">
           <img
-            className="h-10 shadow-lg shadow-gray-900 aspect-square rounded-full"
+            className="h-10 shadow aspect-square rounded-full"
             src={`https://github.com/${import.meta.env.PUBLIC_GITHUB_USER}.png`}
           />
-          <span> etherbeing </span>
         </a>
         {/*<HeaderBreadcrumb title={title} />*/}
       </SpotlightCard>
