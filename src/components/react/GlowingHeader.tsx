58bf0434a7b4cb7f06c74346fd86692dd00dc63d
Esteban Chacon Martin <etherbeing99@proton.me>
2025-12-18 23:48:30 -0500

    feat: Improve contact section design

--- src/components/react/GlowingHeader.tsx
+++ src/components/react/GlowingHeader.tsx
@@ -2,7 +2,7 @@ import GlowingText from "./GlowingText";
 
 export default function GlowingHeader({ children }: { children: string }) {
   return (
-    <div className="text-center animate-fade-up mb-20">
+    <div className="text-center animate-fade-up mb-20 cursor-default select-none">
       <GlowingText>
         <h2 className="text-3xl font-bold">{children}</h2>
         <span> ╰─〔❨✧✧❩〕─╯ </span>
